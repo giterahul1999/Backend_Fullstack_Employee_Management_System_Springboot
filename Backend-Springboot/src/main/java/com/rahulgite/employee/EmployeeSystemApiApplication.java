37a20e7e384651688ad0174a4fb00cd933cca400
Rahul Gite <66781417+giterahul1999@users.noreply.github.com>
2024-10-02 15:56:10 +0530

    Update EmployeeSystemApiApplication.java
    message comment added

--- Backend-Springboot/src/main/java/com/rahulgite/employee/EmployeeSystemApiApplication.java
+++ Backend-Springboot/src/main/java/com/rahulgite/employee/EmployeeSystemApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeSystemApiApplication {
 
 //	main application
+//      message
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSystemApiApplication.class, args);
