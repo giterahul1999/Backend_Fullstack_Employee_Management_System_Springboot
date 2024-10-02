5a4966dc193c487ee6e3737503f57c0507362a6d
Rahul Gite <giterahul1999@gmail.com>
2024-10-02 15:48:53 +0530

    Comment added

--- Backend-Springboot/src/main/java/com/rahulgite/employee/EmployeeSystemApiApplication.java
+++ Backend-Springboot/src/main/java/com/rahulgite/employee/EmployeeSystemApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeSystemApiApplication {
 
+//	main application
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSystemApiApplication.class, args);
 	}
